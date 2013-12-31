0e96a19dcb793370b7a6635b995b6f6b189cd27a
gourgan <gourgan.hicham@gmail.com>
2013-12-31 01:24:25 +0100

    commit d'explication 2
    explication 2

--- src/com/puissancequatre/MainActivity.java
+++ src/com/puissancequatre/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
+		
+		//je fais une modification aussi pour te montrer
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
