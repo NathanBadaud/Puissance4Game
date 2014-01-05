4499cf168c585bd39a708206921cf7b8c25ffb06
gourgan <gourgan.hicham@gmail.com>
2014-01-05 22:52:07 +0100

    play activity

--- src/com/puissancequatre/MainActivity.java
+++ src/com/puissancequatre/MainActivity.java
@@ -1,26 +1,55 @@
 package com.puissancequatre;
 
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//j'ajoute ce commentaire par ex et j'enregistre
-		// un commentaire nimportkoi pour voir
 		setContentView(R.layout.activity_main);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		
-		//je fais une modification aussi pour te montrer
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	 public void two_player(View view) {
 
+	        Intent intent = new Intent(this, Play.class);
+	        startActivity(intent);
+
+	    }
+	    public void one_player(View view) {
+
+	        Intent intent = new Intent(this, Play.class);
+	        startActivity(intent);
+
+	    }
+	    public void score(View view) {
+
+
+	        Intent intent = new Intent(this, Play.class);
+	        startActivity(intent);
+
+	    }
+
+	    public void about(View view) {
+
+	        Intent intent = new Intent(this, Play.class);
+	        startActivity(intent);
+
+	    }
+	    
+	    public void exit(View view) {
+	    	System.exit(0);
+	    }
 }
